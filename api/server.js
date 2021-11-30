d648bd07852789c60d4a12ca6ba4e6b521ca9c4b
Andre_Johnson <Andrejohnson513@gmail.com>
2021-11-29 20:55:13 -0500

    first commit

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('/model')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/hello', (req, res) =>{
+  res.json({message: 'hello'})
+})
 
 module.exports = {}; // EXPORT YOUR SERVER instead of {}
