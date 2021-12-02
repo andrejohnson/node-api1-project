ca35a6477edc9157f96f83889256530daf529e1a
Andre_Johnson <Andrejohnson513@gmail.com>
2021-12-02 01:40:11 -0500

    all test passing

--- api/server.js
+++ api/server.js
@@ -6,6 +6,36 @@ const server = express()
 
 server.use(express.json())
 
+server.put('/api/users/:id', async (req, res) =>{
+  try {
+    const possibleUser = await User.findById(req.params.id)
+    if(!possibleUser){
+      res.status(404).json({
+        message: '/does not exist/',
+       })
+    } else {
+       if (!req.body.name || !req.body.bio){
+         res.status(400).json({
+           message: 'please provide name and bio for the user',
+         })
+      } else {
+         const updatedUser = await User.update(
+            req.params.id,
+            req.body,
+         )
+         res.status(200).json(updatedUser)
+       }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'error getting users',
+      err: err.message,
+      stack: err.stack,
+    
+  })
+        }
+      })
+
 server.get('/api/users', (req, res) =>{
   User.find()
   .then(users =>{
@@ -95,4 +125,4 @@ server.use('*', (req, res) =>{
   })
 })
 
-module.exports = server // EXPORT YOUR SERVER instead of {}
+module.exports = server;
