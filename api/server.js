0b1032c1da455426c14bbf7baf5f1feea277a5d7
Andre_Johnson <Andrejohnson513@gmail.com>
2021-12-02 01:05:34 -0500

    12 of 15 passing

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const User = require('./users/model')
 const server = express()
 
 server.use(express.json())
+
 server.get('/api/users', (req, res) =>{
   User.find()
   .then(users =>{
@@ -18,9 +19,6 @@ server.get('/api/users', (req, res) =>{
   })
   })
 })
-
-
-
 
 server.get('/api/users/:id', (req, res) =>{
   User.findById(req.params.id)
@@ -42,36 +40,53 @@ server.get('/api/users/:id', (req, res) =>{
   })
 })
 
-
-
-
 server.get('/hello', (req, res) =>{
   res.json({message: 'hello'})
 })
 
-server.get('/api/users/:id', async (req,res)=>{
-  try {
-    const user = await User.findById(req.params.id)
-    res.json(user)
-  } catch (err) {
-    res.status(500).json({
-      message:'no go homo',
-      error: err.message
+server.delete('/api/users/:id', async (req,res)=>{
+
+    const possibleUser = await User.findById(req.params.id) 
+     if(!possibleUser) {
+    res.status(404).json({
+      message: "/does not exist/", 
     })
+  } else {
+    const deletedUser = await User.remove(possibleUser.id)
+    res.status(200).json(deletedUser)
   }
 })
 
 server.post('/api/users', async (req, res)=>{
-  try {
-    const newUser = await User.create(req.body)
-    res.status(201).json(newUser)
-  } catch (err) {
-    res.status(500).json({
-      message:'not working',
-      error:err.message
+  const user = req.body;
+  if(!user.name || !user.bio){
+    res.status(400).json({
+      message: "Please provide name and bio for the user"  
     })
-    
+  }else{
+    User.insert(user)
+    .then((stuff) =>{
+        res.status(201).json(stuff)
+    })
+    .catch(err =>{
+      res.status(500).json({
+        message: 'error getting user',
+        err: err.message,
+        stack: err.stack,
+    })
+    })
   }
+  
+  // try {
+  //   const newUser = await User.create(req.body)
+  //   res.status(201).json(newUser)
+  // } catch (err) {
+  //   res.status(500).json({
+  //     message:'not working',
+  //     error:err.message
+  //   })
+    
+  // }
 })
 
 server.use('*', (req, res) =>{
