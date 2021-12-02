d4d8791f18d4f9273cf6a24caaeaa82aea5d4c44
Andre_Johnson <Andrejohnson513@gmail.com>
2021-12-01 23:15:15 -0500

    getting started

--- api/server.js
+++ api/server.js
@@ -1,13 +1,44 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-const User = require('/model')
+const User = require('./users/model')
 
 const server = express()
 
 server.use(express.json())
 
+server.use('*', (req, res) =>{
+  res.status(404).json({
+    message: 'its working'
+  })
+})
+
 server.get('/hello', (req, res) =>{
   res.json({message: 'hello'})
 })
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/user/:id', async (req,res)=>{
+  try {
+    const user = await User.findById(req.params.id)
+    res.json(user)
+  } catch (err) {
+    res.status(500).json({
+      message:'no go homo',
+      error: err.message
+    })
+  }
+})
+
+server.post('/api/users', async (req, res)=>{
+  try {
+    const newUser = await User.create(req.body)
+    res.status(201).json(newUser)
+  } catch (err) {
+    res.status(500).json({
+      message:'not working',
+      error:err.message
+    })
+    
+  }
+})
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
